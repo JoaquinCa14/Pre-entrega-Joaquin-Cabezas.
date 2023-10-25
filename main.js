5f8d0cb6d317ecae011590e98ca2be3458098a21
JoaquinCa14 <121529741+JoaquinCa14@users.noreply.github.com>
2023-10-25 11:55:36 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,47 +1,36 @@
 const productos = {
-    producto1: 10,
-    producto2: 20,
-    producto3: 30,
+    Herbicida: 10,
+    Fungicida: 20,
+    Insecticida: 30,
 }
-
 
 function calcularTotal() {
     let total = 0;
     let continuar = true;
 
     while (continuar) {
-
-        console.log("Productos disponibles:");
+        let mensajeProductos = "Productos disponibles:\n";
         for (let producto in productos) {
-            console.log(`${producto}: $${productos[producto]}`);
+            mensajeProductos += `${producto}: $${productos[producto]}\n`;
         }
-
+        alert(mensajeProductos);
 
         const productoSeleccionado = prompt("Ingrese el nombre del producto seleccionado: ");
 
         if (productoSeleccionado in productos) {
-
             const cantidad = parseInt(prompt("Ingrese la cantidad de unidades: "));
-
-
             const subtotal = productos[productoSeleccionado] * cantidad;
-
-
             total += subtotal;
-
-            console.log(`Subtotal de ${productoSeleccionado}: $${subtotal}`);
+            alert(`Subtotal de ${productoSeleccionado}: $${subtotal}`);
         } else {
-            console.log("Producto no válido. Por favor, seleccione un producto válido.");
+            alert("Producto no válido. Por favor, seleccione un producto válido.");
         }
-
 
         const respuesta = prompt("¿Desea agregar otro producto? (Sí/No): ").toLowerCase();
         continuar = respuesta === "sí" || respuesta === "si";
     }
 
-
-    console.log(`El total de su compra es: $${total}`);
+    alert(`El total de su compra es: $${total}`);
 }
-
 
 calcularTotal();
